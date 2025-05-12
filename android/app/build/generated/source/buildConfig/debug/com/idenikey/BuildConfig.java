e7f9ef31d3a0815d234a410563f84f98d5f3a370
Umer Shahid <umer45769@gmail.com>
2025-05-12 12:32:15 +0500

    Update: Android Build

---
+++ android/app/build/generated/source/buildConfig/debug/com/idenikey/BuildConfig.java
@@ -0,0 +1,16 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.idenikey;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.idenikey";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 36;
+  public static final String VERSION_NAME = "2.9.6";
+  // Field from default config.
+  public static final boolean IS_HERMES_ENABLED = true;
+  // Field from default config.
+  public static final boolean IS_NEW_ARCHITECTURE_ENABLED = false;
+}
